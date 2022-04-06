85994e0a5dc223660394d5f8308724950fc99c1d
YazhRajesh <yazhini321@gmail.com>
2022-04-06 08:15:39 -0700

    Add files via upload

--- examples/rest/main.tf
+++ examples/rest/main.tf
@@ -7,31 +7,40 @@ terraform {
 }
 
 provider "dcnm" {
-  username = ""
-  password = ""
-  url      = ""
-  # expiry   = 900000
+  # Cisco DCNM/NDFC user name
+  username = "{{enter_username}}"
+  # Cisco DCNM/NDFC password
+  password = "{{enter_password}}"
+  # Cisco DCNM/NDFC url
+  url      = "{{enter_url_of_dcnm_or_ndfc}}"
+  insecure = true
+  # Used to select DCNM or NDFC for authentication purposes
+  #nd for NDFC and dcnm for DCNM
+  platform = "nd"
 }
 
-resource "dcnm_rest" "first" {
-  path    = "/rest/top-down/fabrics/fab2/networks"
+resource "dcnm_rest" "new_network" {
+  #To add a new network using REST API for NDFC
+  path    = "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/fab1/networks"
+  method  = "POST"
   payload = <<EOF
-  {
-    "displayName": "ashutosh",
-    "fabric": "fab2",
-    "networkExtensionTemplate": "Default_Network_Extension_Universal",
-    "networkId": "30006",
-    "networkName": "import",
-    "networkTemplate": "Default_Network_Universal",
-    "networkTemplateConfig": "{\"networkName\":\"import\",\"segmentId\":\"30006\",\"vlanId\":2300,\"mtu\":1500,\"gatewayIpAddress\":\"192.0.3.2/24\",\"gatewayIpV6Address\":\"2001:db8::1/64\",\"vlanName\":\"vlan2\",\"intfDescription\":\"second network from terraform\",\"secondaryGW1\":\"192.0.3.1/24\",\"secondaryGW2\":\"192.0.3.1/24\",\"suppressArp\":true,\"mcastGroup\":\"239.1.2.2\",\"dhcpServerAddr1\":\"1.2.3.4\",\"dhcpServerAddr2\":\"1.2.3.4\",\"vrfDhcp\":\"VRF1012\",\"loopbackId\":100,\"tag\":\"1400\",\"trmEnabled\":true,\"rtBothAuto\":true,\"enableL3OnBorder\":true}",
-    "vrf": "MyVRF"
-  }
- EOF 
-}
 
-resource "dcnm_rest" "template_validate" {
-  path         = "/rest/config/templates/validate"
-  method       = "POST"
-  payload      = file("payload.txt")
-  payload_type = "text"
+    {
+      "displayName": "Terraform_Network",
+      "fabric": "fab1",
+      "hierarchicalKey": "fab1",
+      "id": 2,
+      "interfaceGroups": null,
+      "networkExtensionTemplate": "Default_Network_Extension_Universal",
+      "networkId": 30003,
+      "networkName": "Terraform_Network",
+      "networkTemplate": "Default_Network_Universal",
+      "networkTemplateConfig": "{\"suppressArp\":\"false\",\"loopbackId\":\"\",\"enableL3OnBorder\":\"false\",\"SVI_NETFLOW_MONITOR\":\"\",\"enableIR\":\"false\",\"isLayer2Only\":\"false\",\"vrfDhcp3\":\"\",\"segmentId\":\"30003\",\"ENABLE_NETFLOW\":\"false\",\"dhcpServerAddr3\":\"\",\"vrfDhcp2\":\"\",\"dhcpServerAddr2\":\"\",\"tag\":\"12345\",\"dhcpServerAddr1\":\"\",\"nveId\":\"1\",\"vrfDhcp\":\"\",\"vlanId\":\"\",\"gatewayIpAddress\":\"172.30.102.1/24\",\"vlanName\":\"\",\"mtu\":\"\",\"intfDescription\":\"\",\"mcastGroup\":\"239.1.1.1\",\"trmEnabled\":\"\",\"VLAN_NETFLOW_MONITOR\":\"\",\"vrfName\":\"Terraform_VRF\"}",
+      "serviceNetworkTemplate": null,
+      "source": null,
+      "tenantName": null,
+      "vrf": "Terraform_VRF"
+    }
+
+  EOF
 }
